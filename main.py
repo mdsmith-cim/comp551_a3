df0e7556f39bb93fbfc7aeebdf7dba5ead261787
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-10-28 17:12:56 -0400

    Working SIFT but need to speed up and/or add save/load functionality

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ pp = preprocess()
 # Load from disk, threshold
 trainX, trainY, testX = pp.get_clean_data()
 
+# SIFT Example
 
+trainSIFTX, trainY, testSIFTX = pp.get_sift_features()
--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 import pandas as pd
+from cv2 import xfeatures2d
+from cv2 import KeyPoint
+from tqdm import tqdm
 
 class preprocess:
 
@@ -44,6 +47,42 @@ class preprocess:
         trainLabels = pd.read_csv(self.train_y_file)['Prediction'].values
 
         return trainData, trainLabels, testData
+
+    def get_sift_features(self, step_size=5):
+
+        trainX, trainY, testX = self.get_clean_data()
+
+        sft = xfeatures2d.SIFT_create()
+
+        imgSize = trainX.shape[-2:]
+
+        # Create keypoint list
+        kp = [KeyPoint(x, y, step_size) for y in range(0, imgSize[0], step_size)
+              for x in range(0, imgSize[1], step_size)]
+
+        print("Computing SIFT features [training]")
+
+        trainXSIFT = np.zeros((trainX.shape[0], len(kp), sft.descriptorSize()), dtype='uint8')
+        testXSIFT = np.zeros((testX.shape[0], len(kp), sft.descriptorSize()), dtype='uint8')
+
+        for i in tqdm(range(trainX.shape[0])):
+            img = trainX[i]
+
+            dsift = sft.compute(img, kp)[1]
+            trainXSIFT[i] = dsift
+
+        print("Computing SIFT features [testing]")
+
+        for i in tqdm(range(testX.shape[0])):
+            img = testX[i]
+
+            dsift = sft.compute(img, kp)[1]
+            testXSIFT[i] = dsift
+
+        print("Done!")
+
+        return trainXSIFT, trainY, testXSIFT
+
 
     def writeToDisk(self, data, filename='data.bin'):
 
