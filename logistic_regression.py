9dc2890ccb40b0110b1e06ad15ec4e41d1da9e7a
Michael Smith <michael.smith6@mail.mcgill.ca>
2016-11-05 15:21:17 -0400

    logistic regression CV code for report

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,17 +2,39 @@ from preprocess import preprocess
 from data_utils import load_data
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+import joblib
 
-if __name__ == '__main__':
+trainX, trainY, testX = load_data()
 
-    trainX, trainY, testX = load_data()
+# --------- Clean data
+pipe = make_pipeline(preprocess('clean', flatten=True, center=True, closing=False, normalize=True),
+                     LogisticRegression(solver='sag', n_jobs=-1, verbose=60))
 
-    X_train, X_valid, y_train, y_valid = train_test_split(trainX, trainY, test_size=0.3, random_state=984930)
+params = dict(logisticregression__C=[0.01, 0.1, 1, 10, 100])
 
-    pipe = make_pipeline(preprocess('sift', flatten=True), LogisticRegression(solver='sag', n_jobs=-1, verbose=60))
+grid = GridSearchCV(pipe, n_jobs=-1, verbose=50, param_grid=params)
 
-    pipe.fit(X_train, y_train)
+grid.fit(trainX, trainY)
 
-    print("Validation score: {0}".format(pipe.score(X_valid, y_valid)))
+print("Results with clean data:\n{0}".format(grid.cv_results_))
 
+joblib.dump(grid, "log_reg_CV_clean.pk")
+
+# -------- SIFT
+
+# Reload data just in case through a bug original data is modified
+trainX, trainY, testX = load_data()
+
+pipe = make_pipeline(preprocess('sift', flatten=True, center=True, closing=False, normalize=True),
+                     LogisticRegression(solver='sag', n_jobs=-1, verbose=60))
+
+params = dict(logisticregression__C=[0.01, 0.1, 1, 10, 100])
+
+grid = GridSearchCV(pipe, n_jobs=-1, verbose=50, param_grid=params)
+
+grid.fit(trainX, trainY)
+
+print("Results with SIFT data:\n{0}".format(grid.cv_results_))
+
+joblib.dump(grid, "log_reg_CV_sift.pk")
